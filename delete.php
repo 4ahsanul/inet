5714adb88bd7fb349679ca9b697c5b0d4bda108a
Yulfiz <58451478+4ahsanul@users.noreply.github.com>
2021-06-20 09:39:35 +0700

    Create delete.php
    Pembuatan file delete.php, untuk menghapus data yang sudah disimpan didalam db_pegawai

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php 
+include "config.php";
+
+// Jika variabel id diset di URL, kita dapat mengedit
+if (isset($_GET['id'])) {
+	$user_id = $_GET['id'];
+
+	// Tulis delete query
+	$sql = "DELETE FROM `users` WHERE `id`='$user_id'";
+
+	// Eksekusi the query
+
+	$result = $conn->query($sql);
+
+	if ($result == TRUE) {
+		echo "Record deleted successfully.";
+	}else{
+		echo "Error:" . $sql . "<br>" . $conn->error;
+	}
+}
+
+?>
