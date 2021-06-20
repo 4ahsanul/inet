e6ebdc00bcc4042d302f9e9f0c00f0e868f2da72
Yulfiz <58451478+4ahsanul@users.noreply.github.com>
2021-06-20 08:36:01 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root"; // default username for localhost is root
-$password = ""; // default password for localhost is empty
-$dbname = "db_pegawai"; // database name
+$username = "root"; // Default username dari localhost adalah root
+$password = ""; // Default password dari localhost adalah kosong/empty
+$dbname = "db_pegawai"; // Database name yang akan digunakan
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
